ae4d0df1099d354d15e08ec1e347d61ab76f0e1a
Moraes <pedro.moraes@accountfy.com>
2022-05-10 22:29:44 -0300

    Adicionando card de produtos

--- cardapio-digital/src/app/app.module.ts
+++ cardapio-digital/src/app/app.module.ts
@@ -7,17 +7,19 @@ import { TelaInicioComponent } from './components/tela-inicio/tela-inicio.compon
 import { HeaderComponent } from './components/header/header.component';
 import { CardapioService } from './services/ cardapio.service';
 import { HttpClientModule } from '@angular/common/http';
+import { CardInfoComponent } from './components/card-info/card-info.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TelaInicioComponent,
     HeaderComponent,
+    CardInfoComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
   ],
   providers: [CardapioService],
   bootstrap: [AppComponent]
--- cardapio-digital/src/app/components/components.module.ts
+++ cardapio-digital/src/app/components/components.module.ts
@@ -2,8 +2,9 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HeaderComponent } from './header/header.component';
+import { CardInfoComponent } from './card-info/card-info.component';
 @NgModule({
-  declarations: [HeaderComponent],
+  declarations: [HeaderComponent, CardInfoComponent],
   imports: [CommonModule, FormsModule],
   exports: [],
 })
